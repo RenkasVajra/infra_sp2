139a55be0f9125626f55658be6ccbc3e551ab8ed
Andrey <twinka4ik174@mail.ru>
2021-04-14 18:53:09 +0500

    PEP1.0

--- api/permissions.py
+++ api/permissions.py
@@ -2,25 +2,34 @@ from rest_framework import permissions
 
 
 class ReviewCommentPermission(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return (request.method in permissions.SAFE_METHODS or
-                request.user.is_authenticated)
+    def __init__(self, request, view, obj):
+        self.request = request
+        self.view = view
+        self.obj = obj
 
-    def has_object_permission(self, request, view, obj):
-        if (request.method in permissions.SAFE_METHODS or
-                obj.author == request.user):
+    def has_permission(self):
+        return (self.request.method in permissions.SAFE_METHODS or
+                self.request.user.is_authenticated)
+
+    def has_object_permission(self):
+        if (self.request.method in permissions.SAFE_METHODS or
+                self.obj.author == self.request.user):
             return True
         return (
-            request.method in permissions.SAFE_METHODS or
-            obj.author == request.user or
-            request.user.role == request.user.Role.MODERATOR or
-            request.user.role == request.user.Role.ADMIN
+            self.request.method in permissions.SAFE_METHODS or
+            self.obj.author == self.request.user or
+            self.request.user.role == self.request.user.Role.MODERATOR or
+            self.request.user.role == self.request.user.Role.ADMIN
         )
 
 
 class IsSuperUserOrReadOnly(permissions.BasePermission):
-    def has_permission(self, request, view):
+    def __init__(self, request, view):
+        self.request = request
+        self.view = view
+
+    def has_permission(self):
         return (
-            request.method in permissions.SAFE_METHODS or
-            request.user.is_superuser
+            self.request.method in permissions.SAFE_METHODS or
+            self.request.user.is_superuser
         )
